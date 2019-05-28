a542aade3150b4309db70a5c8d0c3a4a9b5b7178
dllud <dllud@riseup.net>
2019-05-28 18:56:33 +0100

    StoreEntity is now mutable.
    This allows PasswordStore to keep its current logic.
    In the future we should refactor the code to have it as immutable again.

--- app/src/main/java/com/zeapo/pwdstore/db/entity/StoreEntity.kt
+++ app/src/main/java/com/zeapo/pwdstore/db/entity/StoreEntity.kt
@@ -1,14 +1,16 @@
 package com.zeapo.pwdstore.db.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "store")
 data class StoreEntity (
-    @PrimaryKey val name: String,
-    val path: String,
-    val external: Boolean,
-    val initialized: Boolean,
-    @Embedded val gitRemote: GitRemote?
+        @PrimaryKey val name: String,
+        var path: String,
+        var external: Boolean,
+        var initialized: Boolean,
+        @ColumnInfo(name = "repo_changed") var repoChanged: Boolean,
+        @Embedded var gitRemote: GitRemote?
 )
